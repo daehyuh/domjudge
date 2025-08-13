c88c87802a5042d7b61347bed01fb50d4430e169
daehyuh <rkdeown10@naver.com>
2025-08-13 21:22:34 +0900

    test

--- start.sh
+++ start.sh
@@ -3,4 +3,10 @@ rm checker/.env
 sudo echo "RESTAPI_PASSWORD=$(sudo sh checkpass.sh 2>&1 | tr -s ' \n' ' ' | awk '{print $NF}')" >> checker/.env
 sudo docker compose -f checker/docker-compose-checker.yml up -d
 
-docker exec -it domserver cat /opt/domjudge/domserver/etc/initial_admin_password.secret
+
+
+echo 어드민 패스워드 확인 중...
+echo ID : admin
+echo password : $(sudo docker exec -it domserver cat /opt/domjudge/domserver/etc/initial_admin_password.secret)
+
+hostname -I | awk '{print $1}':12345
