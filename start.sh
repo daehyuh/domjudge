64f39b30f39e2e620add19abea13fc21e5030cf0
daehyuh <rkdeown10@naver.com>
2025-08-13 21:18:39 +0900

    test

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 sudo docker compose -f domjudge/docker-compose.yml up -d
-sudo echo "RESTAPI_PASSWORD=$(sudo sh checkpass.sh | awk 'NF {print $NF}')" >> checker/.env
+sudo echo "RESTAPI_PASSWORD=$(sudo sh checkpass.sh 2>&1 | tr -s ' \n' ' ' | awk '{print $NF}')" >> checker/.env
 sudo docker compose -f checker/docker-compose-checker.yml up -d
