041415af00e3b843599078492ad05a34d69f22ec
daehyuh <rkdeown10@naver.com>
2025-08-13 21:20:40 +0900

    test

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@ sudo docker compose -f domjudge/docker-compose.yml up -d
 rm checker/.env
 sudo echo "RESTAPI_PASSWORD=$(sudo sh checkpass.sh 2>&1 | tr -s ' \n' ' ' | awk '{print $NF}')" >> checker/.env
 sudo docker compose -f checker/docker-compose-checker.yml up -d
+
+docker exec -it domserver cat /opt/domjudge/domserver/etc/initial_admin_password.secret
