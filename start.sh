4c60d5a6dc0da372ca134371ec34f06d1d9ea2f6
daehyuh <rkdeown10@naver.com>
2025-08-13 21:07:56 +0900

    test

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-sudo docker compose start mariadb domserver
+sudo docker compose -f domjudge/docker-compose.yml start mariadb domserver
 
-sudo echo "RESTAPI_PASSWORD=$(sudo sh checkpass.sh | awk 'NF {print $NF}')" >> db.env
+sudo echo "RESTAPI_PASSWORD=$(sudo sh checkpass.sh | awk 'NF {print $NF}')" >> checker/.env
 
-sudo docker compose -f docker-compose-checker.yml up -d
+sudo docker compose -f checker/docker-compose-checker.yml up -d
--- stop.sh
+++ stop.sh
@@ -1,1 +1,2 @@
-sudo docker compose stop mariadb domserver
+sudo docker compose -f domjudge/docker-compose.yml stop mariadb domserver
+sudo docker compose -f checker/docker-compose-checker.yml stop
