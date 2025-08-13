8edf057504fbd45eebcfb82ef66bba5c0cdfce1e
daehyuh <rkdeown10@naver.com>
2025-08-13 20:58:49 +0900

    test

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 sudo docker compose start mariadb domserver
+
+sudo echo "RESTAPI_PASSWORD=$(sudo sh checkpass.sh | awk 'NF {print $NF}')" >> .env
+
 sudo docker compose -f docker-compose-checker.yml up -d
